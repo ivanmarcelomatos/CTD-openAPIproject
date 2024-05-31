1f74955542919262b6395241c79e772484db9f5a
ivanmarcelomatos <ivanmarcelomatos@gmail.com>
2024-05-31 10:44:46 -0300

    added images, description, ingredients and styling

--- js-file.js
+++ js-file.js
@@ -5,17 +5,10 @@ fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         
+        console.log(typeof data);
         console.log(data);
-        // console.log(typeof repodata
-        // Step 2: Display Repositories in List
-        const projectSection = document.getElementById('Projects');
-        const projectList = projectSection.querySelector('ul');
 
-        for (const item of data) {
-            const project = document.createElement('li');
-            project.innerText = item.title;
-            projectList.appendChild(project);
-        }
+        populateCoffees(data);
     })
     .catch(error => {
         console.error('Error fetching the repos:', error);
@@ -26,3 +19,34 @@ fetch(apiUrl)
         ==> ${error}`;
         projectSection.appendChild(errorMessage);
     });
+
+    function populateCoffees(obj) {
+        const projectSection = document.getElementById('Projects');
+        const coffeesDiv = projectSection.querySelector('div');
+
+        for (const item of obj) {
+            const myArticle = document.createElement('article');
+            const myH3 = document.createElement('h3');
+            const myPara1 = document.createElement('p');
+            const myArticleImg = document.createElement('img');
+            const myUl = document.createElement('ul');
+            
+
+            myH3.textContent = item.title;
+            myPara1.textContent = item.description;
+            myArticleImg.src = item.image;
+            
+            for (let i = 0; i < item.ingredients.length; i++) {
+                const myLi = document.createElement('li');
+                myLi.textContent = item.ingredients[i];
+                myUl.appendChild(myLi);
+            };
+            
+
+            myArticle.appendChild(myH3);
+            myArticle.appendChild(myArticleImg);
+            myArticle.appendChild(myPara1);
+            myArticle.appendChild(myUl);
+            coffeesDiv.appendChild(myArticle);
+        }
+    };
